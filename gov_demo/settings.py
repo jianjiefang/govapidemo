e501baa208b9955ffcde93a80aeeb1c1672d34e2
jianjiefang <42630557+jianjiefang@users.noreply.github.com>
2019-04-28 13:52:36 +0800

    修改为MySQL数据库

--- gov_demo/settings.py
+++ gov_demo/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'gov_demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'govapidemo',
+        'USER': 'root',
+        'PASSWORD': 'Fbac2018!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
